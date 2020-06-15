1c49c058421f6c2372c002475a3292bd411efc3f
Santistrella <santistrella@gmail.com>
2020-06-15 16:38:30 +0200

    first commit

---
+++ src/Components/Explore/Explore.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import './Explore.css';
+
+
+
+
+
+export const Explore = () => {
+    return (
+        <div className="explore">
+            <h1>Explore</h1>
+        </div>
+    );
+}
---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import './Profile.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
+export const Profile = () => {
+    return (
+        <div className="profile">
+            <h1>Profile</h1>
+        </div>
+    );
+}
+
