cc4f438923578a7d1591050c739742927a856dc1
Santistrella <santistrella@gmail.com>
2020-06-22 16:54:32 +0200

    exploreladingpage

---
+++ front-end/src/Components/Log/Logged.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+
+import {TopNav} from "../TopNav/Topnav";
+import {Header} from "../Header/Header";
+
+
+
+
+export const Logged = () => {
+    return (
+        <div>
+            <Header />
+            <TopNav/>
+        </div>
+    );
+}
