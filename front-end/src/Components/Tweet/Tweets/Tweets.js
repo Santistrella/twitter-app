f607828ab7a421d5aaef11102ac26af9ddc96209
Santistrella <santistrella@gmail.com>
2020-07-20 21:23:09 +0200

    Added redirect when user isn't logged + fixed some visual stuff + new component

--- front-end/src/Components/Tweet/Tweets/Tweets.js
+++ front-end/src/Components/Tweet/Tweets/Tweets.js
@@ -6,6 +6,7 @@ import {
   faCommentAlt,
   faHeart,
   faShareSquare,
+  faTrashAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import profilepic from "./profilepic.jpg";
 
@@ -40,8 +41,25 @@ export const Tweets = (tweet) => {
     return <div />;
   }
 
+  const DeleteTweet = () => {
+    const token = localStorage.getItem("user");
+    fetch(`http://localhost/api/tweet/${tweet.id}`, {
+      method: "delete",
+      mode: "cors",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw res;
+    });
+  };
+
   return (
-    <div className="tweetContainer">
+    <div className="tweetContainer" key={tweet.id}>
       <header>
         <img src={profilepic} className="profile-thumbnail" />
         <div className="profile-name">
@@ -65,6 +83,15 @@ export const Tweets = (tweet) => {
           </button>
           <button id="SendBtn">
             <FontAwesomeIcon icon={faShareSquare} />
+          </button>
+          <button
+            className="LikeBtn"
+            id="deleteBtn"
+            onClick={() => {
+              DeleteTweet(tweet.id);
+            }}
+          >
+            <FontAwesomeIcon icon={faTrashAlt} />
           </button>
         </div>
       </footer>
