6de9a66c0c3c0433c5d81f691f62adfc3092e11d
albert <albert.figuerola.gmz@gmail.com>
2020-09-09 21:05:09 +0200

    context auth in tweet context

--- front-end/src/Components/Tweet/TweetContext.js
+++ front-end/src/Components/Tweet/TweetContext.js
@@ -1,11 +1,19 @@
 import React, { createContext, useCallback, useContext, useState } from "react";
+import {useAuth} from "../../Context/authentication.context";
 
 const TweetContext = createContext();
 
 const TweetContextProvider = (props) => {
   const [tweets, setTweets] = useState(undefined);
+  const { auth } = useAuth();
   const refresh = useCallback(() => {
-    fetch("http://localhost/api/tweet")
+    fetch("http://localhost/api/tweet",{
+      mode: "cors",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${auth.token}`,
+      },
+    })
       .then((response) => response.json())
       .then((res) => {
         setTweets(res);
