9b1303dea55bcb3dda471f5ea30321389139b507
SantiStrella <62253098+Santistrella@users.noreply.github.com>
2020-12-20 18:27:20 +0100

    Update Wrapper.js

--- front-end/src/Api/Wrapper.js
+++ front-end/src/Api/Wrapper.js
@@ -1,6 +1,6 @@
 let API_HOST = 'http://localhost'; //localhost sin nada más hace referencia al puerto 80, que me sirve para atacar al backend
 if ( window.location.hostname !== "localhost" ) {
-    API_HOST = "https://pepe-trello.herokuapp.com"
+    API_HOST = "https://tweetah.herokuapp.com"
 }
 export const fetchResource = (resourceName, userOptions = {}, id, params = {}) => { //el resource name es lo que va despues de la / tras localhost. Las options son las opciones del fetch (mode, headers...).
     const token = localStorage.getItem('user');
